33a7fe2f81a67dede9c9a94d86277f0f74a738bb
mapapicv@gmail.com <mapapicv@gmail.com>
2025-11-02 15:28:48 -0300

    debugging experiencias

--- app/src/main/java/com/appecoviaje/data/UserPreferencesRepository.kt
+++ app/src/main/java/com/appecoviaje/data/UserPreferencesRepository.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -35,3 +36,6 @@ class UserPreferencesRepository(private val dataStore: DataStore<Preferences>) {
         }
     }
 }
+
+//  Agregar esto fuera de la clase
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_prefs")
